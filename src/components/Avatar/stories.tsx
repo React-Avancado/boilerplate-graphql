2d2cc875f839c9ec55cae641f3946b4fc48fc47d
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2022-02-01 13:55:36 -0300

    Bump packages

--- src/components/Avatar/stories.tsx
+++ src/components/Avatar/stories.tsx
@@ -3,7 +3,14 @@ import Avatar, { AvatarProps } from '.'
 
 export default {
   title: 'Avatar',
-  component: Avatar
+  component: Avatar,
+  argTypes: {
+    name: {
+      control: {
+        type: 'text'
+      }
+    }
+  }
 } as Meta
 
 export const Default: Story<AvatarProps> = (args) => <Avatar {...args} />
